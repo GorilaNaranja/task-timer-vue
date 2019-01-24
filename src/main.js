49af5204e5b033805d171da72cd97de8d36d467d
GorilaNaranja <36081861+GorilaNaranja@users.noreply.github.com>
2019-01-24 18:21:29 +0000

    Add buefy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+// Styles from bulma
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
