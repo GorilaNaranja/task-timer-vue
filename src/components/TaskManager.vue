d5932d7ab1476fc21653dec25bab543d1801f405
GorilaNaranja <36081861+GorilaNaranja@users.noreply.github.com>
2019-01-24 18:20:41 +0000

    Create TaskManager component

---
+++ src/components/TaskManager.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <app-task
+      v-for="(task, index) in tasks"
+      @click.native="deleteTask(index)"
+      @remove="$emit('delete-task', index)"
+      :task="tasks[index]"></app-task>
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+  props:['tasks'],
+  components:{
+    appTask: Task
+  },
+  data() {
+    return {
+      isActive:true
+    }
+  },
+  methods: {
+      deleteTask(index) {
+        if(!this.isActive){
+          this.$emit('deleteTaskEvent', index);
+        }
+      },
+      changeToDeleteable(event){
+        this.isActive = event;
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
