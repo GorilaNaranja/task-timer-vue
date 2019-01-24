3796d3a5a5e9602dfee0f3d1159f74b1920c403a
GorilaNaranja <36081861+GorilaNaranja@users.noreply.github.com>
2019-01-24 18:20:31 +0000

    Create TaskCreator component

---
+++ src/components/TaskCreator.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="creator-box">
+    <p> Hello i'm the task creator.</p>
+    <div class="box-divider">
+      <div class="column-55">
+        <label> Task message </label>
+        <b-input v-model="msg"></b-input>
+      </div>
+      <div class="column-15">
+        <label> Task time </label>
+        <b-input v-model="time" type="number"></b-input>
+      </div>
+      <div class="column-15">
+        <label> Format time </label>
+        <b-select placeholder="Select a time" v-model="selected">
+          <option
+            v-for="(format, index) in formats" :value="format">
+            {{ format.name }}
+          </option>
+        </b-select>
+      </div>
+      <div class="column-rest">
+        <button @click.prevent="createTask" class="button is-info">Create new Task</button>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      msg:'',
+      time:0,
+      selected:{},
+      formats:[
+        {
+          name:'miliseconds',
+          multiplier:1
+        },{
+          name:'seconds',
+          multiplier:1000
+        },{
+          name:'minutes',
+          multiplier:60000
+        }
+      ]
+    }
+  }, methods:{
+    createTask: function(){
+      this.$emit('addTask',
+        {
+          name:this.msg,
+          time:this.time,
+          multiplier: this.selected.multiplier
+        }
+      );
+      // this.msg='';
+      // this.time=0;
+    }
+  }
+}
+</script>
+
+<style>
+.box-divider{
+  display:flex;
+  width: 100%;
+}
+.column-15{
+  width: 15%;
+  margin: 10px;
+}
+.column-55{
+  width: 55%;
+  margin: 10px;
+}
+.column-rest{
+  width:auto;
+  margin:10px;
+  align-self:flex-end;
+}
+.creator-box{
+  background: #f6f6f6;
+  padding:10px;
+  margin:10px;
+}
+</style>
