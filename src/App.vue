ad48234c37adcf5f7cde7eb6d114a450d5c958f3
GorilaNaranja <36081861+GorilaNaranja@users.noreply.github.com>
2019-01-24 18:21:44 +0000

    Import components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 style="font-size:30px; margin:20px;"> Welcome to Task Timer App</h1>
+    <app-global-header :tasks="tasks"></app-global-header>
+    <app-task-creator @addTask="newTask"></app-task-creator>
+    <app-task-manager :tasks="tasks" @delete-task="deleteTask"></app-task-manager>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GlobalHeader from './components/GlobalHeader.vue'
+import TaskCreator from './components/TaskCreator.vue'
+import TaskManager from './components/TaskManager.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    appGlobalHeader: GlobalHeader,
+    appTaskCreator: TaskCreator,
+    appTaskManager: TaskManager
+  },
+  data: function(){
+    return {
+      tasks:[]
+    }
+  }, methods:{
+    newTask(task){
+      this.tasks.push(task);
+    },
+    deleteTask(index){
+      this.tasks.splice(index, 1);
+    }
   }
 }
 </script>
@@ -23,6 +41,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
 </style>
