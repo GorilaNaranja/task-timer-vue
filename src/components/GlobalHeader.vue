3fc96f95375918529ade5e704d2be36b95b16d86
GorilaNaranja <36081861+GorilaNaranja@users.noreply.github.com>
2019-01-24 18:20:05 +0000

    Create GlobalHeader component

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="creator-box">
+    <p> We have {{ tasks.length }} tasks running </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['tasks']
+}
+</script>
+
+<style>
+</style>
